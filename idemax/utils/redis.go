8d922f8cc306c5d04414377608863b26a8bf071b
Alex Sahm <alexander.sahm@gmx.de>
2025-02-22 13:26:20 +0100

    rework redis client initialization
    - Redis connection is initialized only once and reused across all functions.
    - Eliminates redundant Redis client creation.
    - Improves performance and stability.

--- idemax/utils/redis.go
+++ idemax/utils/redis.go
@@ -2,18 +2,40 @@ package utils
 
 import (
 	"os"
+	
+	"sync"
 
+	
 	"github.com/redis/go-redis/v9"
 )
 
-func GetRedisClient() *redis.Client {
-	addr := os.Getenv("REDIS_HOST")
-	if addr == "" {
-		addr = "localhost:6379"
-	}
 
-	return redis.NewClient(&redis.Options{
-		Addr: addr,
-		DB:   0,
+
+var (
+	redisClient *redis.Client
+	once        sync.Once
+)
+
+// Initializee Redis-connection only once
+func InitRedis() {
+	once.Do(func() {
+		addr := os.Getenv("REDIS_HOST")
+		if addr == "" {
+			addr = "localhost:6379"
+		}
+
+		redisClient = redis.NewClient(&redis.Options{
+			Addr: addr,
+			DB:   0,
+		})
 	})
 }
+
+// GetRedisClient returns global instance of redis client
+func GetRedisClient() *redis.Client {
+	if redisClient == nil {
+		InitRedis()
+	}
+	return redisClient
+}
+
